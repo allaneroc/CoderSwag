a2b3caf8ba49dc397ef0341582e442ad486ce730
allaneroc <roc.allane@live.com>
2018-10-29 12:47:07 -0400

    Lesson 01

--- app/src/main/java/com/allaneroc/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/allaneroc/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.allaneroc.coderswag
+package com.allaneroc.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.allaneroc.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
